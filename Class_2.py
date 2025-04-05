9f6609a64196ac596a7b993dabefd4ac20ce75d4
Archimedes <dineshvignesh179@gmail.com>
2025-04-05 17:51:18 +0530

    set

--- Class_2.py
+++ Class_2.py
@@ -15,13 +15,23 @@
 # fruit_list = ["apple" , "banana" , "cherry", "date"]
 # print(fruit_list)
 
-dict_new = {"class":"A" ,"class":"B","class":"c","age":32 }
-print(dict_new)
-dict_new.pop("age")
-print(dict_new)
-dict_new["email"] = "dineshvignesh179@gmail.com"
-print(dict_new)
-# del - predifine function to delet in dictonaires
-del dict_new["class"]
-print(dict_new)
+# dict_new = {"class":"A" ,"class":"B","class":"c","age":32 }
+# print(dict_new)
+# dict_new.pop("age")
+# print(dict_new)
+# dict_new["email"] = "dineshvignesh179@gmail.com"
+# print(dict_new)
+# # del - predifine function to delet in dictonaires
+# del dict_new["class"]
+# print(dict_new)
 
+
+#set
+new_set = {1,2,3,4,5,1,2,4,5,6,7,7}
+print(type(new_set))
+print(new_set)
+# print(new_set[1]) -- error(immutable)
+ABC= list(new_set)
+print(ABC)
+ABC.append(8)
+print(ABC)
