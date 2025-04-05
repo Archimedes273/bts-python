89c697ca6352aecdc32b9b2061a0f1817936f945
Archimedes <dineshvignesh179@gmail.com>
2025-04-05 17:35:09 +0530

    Python_class2

---
+++ Class_1_Intro.py
@@ -0,0 +1,58 @@
+# Name = "Dinesh"
+# Age = 27
+# Height = 5.6
+# Is_Student = False
+#
+# print(Name,Age,Height,Is_Student)
+
+# a= 10
+# b = 20
+# temp = a
+# a = b
+# b = temp
+# print(a,b)
+
+# a = input("Enter first number : ")
+# b = input("Enter Second number :")
+# print ("Sum:" ,int(a)+int(b))
+
+# a = 10
+# b = 5
+# a,b = b,a
+# print("After swapping :", a,b);
+
+# Name = "Dinesh"
+# Age = 27
+# Height = 5.6
+# Is_Student = False
+#
+# print("Type of name:" , type(Name))
+# print("Type of age:", type(Age))
+# print("Type of Height:", type(Height))
+# print("Type of Is_student:", type(Is_Student))
+
+# l = input("Length :")
+# w = input("width :")
+# Area = int(l) * int(w)
+# print("Area of rectangle :", Area)
+
+# x,y,z = 5,10,"Hello"
+# print(x,y,z,sep="\n")
+
+
+# a = int(input("Enter a number:"))
+# if(a>=10):
+#     print("True")
+# else:
+#     print("False")
+
+a = int(input("enter number1:"))
+b = int(input("enter number2:"))
+print(a>b)
+
+
+
+
+
+
+
---
+++ Class_2.py
@@ -0,0 +1,27 @@
+# List
+# Tuple
+# Dictionaries
+# Sets
+
+
+#list
+# fruits = ["apple" , "orange" , "cherry"]
+# print(fruits[0])
+# fruits.append("grapes")
+# print(fruits)
+# fruits.remove("orange")
+# print(fruits)
+
+# fruit_list = ["apple" , "banana" , "cherry", "date"]
+# print(fruit_list)
+
+dict_new = {"class":"A" ,"class":"B","class":"c","age":32 }
+print(dict_new)
+dict_new.pop("age")
+print(dict_new)
+dict_new["email"] = "dineshvignesh179@gmail.com"
+print(dict_new)
+# del - predifine function to delet in dictonaires
+del dict_new["class"]
+print(dict_new)
+
